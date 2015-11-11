23ecf1b06411d2f49b8654e5a86596cbd4205828
Glenn <mr.sharpoblunto@gmail.com>
2015-11-11 09:59:06 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # restartable-process
 Spawn a process and provide a means to easily restart it
+
+##Usage
+```javascript
+var Restartable = require('restartable-process');
+
+// new Restartable(processName,args,options)
+// start up a node process with the args 'my-app/index.js'
+// and set the NODE_ENV environment variable.
+// NOTE: the options object is identical to what you would
+// pass when spawning a process using nodes built in spawn function
+var process = new Restartable('node',['my-app/index.js'],{
+  env: { NODE_ENV: 'production' }
+});
+
+// Start the process if it's not already started or restart it 
+// if it is already started.
+process.restart(function() {
+  console.log('process restarted');
+});
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+var spawn = require('child_process').spawn;
+
+var RestartableProcess = function(processName,args,opts) {
+    this._processName = processName;
+    this._args = args;
+    this._opts = Object.assign(opts || {},{ stdio: 'inherit' });
+    this._onRestart = null;
+    process.on('exit',()=> {
+        if (this._process) {
+            this._process.kill();
+        }
+    });
+};
+
+RestartableProcess.prototype._start = function(cb) {
+    this._process = spawn(this._processName,this._args,this._opts);
+    this._process.on('close',function(code) {
+        this._process = null;
+        if (this._onRestart) {
+            cb = this._onRestart;
+            this._onRestart = null;
+            this._start(cb);
+        }
+    }.bind(this));
+    cb();
+};
+
+RestartableProcess.prototype.restart = function(cb) {
+    if (this._process) {
+        this._onRestart = cb;
+        this._process.kill();
+    } else {
+        this._start(cb);
+    }
+};
+
+module.exports = RestartableProcess;
